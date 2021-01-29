a3fa50cd64affa750733f06a18c43948a16c0665
Capelaum <thecapellett@gmail.com>
2021-01-29 12:18:17 -0300

    add favicon

--- exibicao/principal.php
+++ exibicao/principal.php
@@ -5,6 +5,7 @@
   <!-- Meta tags Obrigatórias -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="shortcut icon" href="exibicao/favicon.ico" type="image/x-icon">
 
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
